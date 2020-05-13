035a785485ac8e2a1958e6c304860dbde8135b69
Gavin Sutton <gavin.w.c.sutton@gmail.com>
2020-05-13 12:34:24 -0400

    Updates

---
+++ precache-manifest.5dd489a2b3c8f6508f13e0884314a03a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d84a02ec2d866dc5932339a8a61d86ca",
+    "url": "/flickPicker/index.html"
+  },
+  {
+    "revision": "90c66c3c1d91b3317954",
+    "url": "/flickPicker/static/css/main.4fe4a00f.chunk.css"
+  },
+  {
+    "revision": "83dfb8e23ae99d7c4fee",
+    "url": "/flickPicker/static/js/2.8b70cb0d.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/flickPicker/static/js/2.8b70cb0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c66c3c1d91b3317954",
+    "url": "/flickPicker/static/js/main.e12d0e9f.chunk.js"
+  },
+  {
+    "revision": "6060d9eed68a09fbc633",
+    "url": "/flickPicker/static/js/runtime-main.e513f3a0.js"
+  },
+  {
+    "revision": "74bddd9efd340a7e5bfae93e4feac102",
+    "url": "/flickPicker/static/media/errorPoster.74bddd9e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gavin-sutton-project-five/precache-manifest.442252028c2fdb852ffbe1a2f84f18f8.js"
+  "/flickPicker/precache-manifest.5dd489a2b3c8f6508f13e0884314a03a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gavin-sutton-project-five/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/flickPicker/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
